aed8fb3a986304e02434add39a9d16ec6da87617
Haytham KATBY <mohakapt@gmail.com>
2023-06-16 15:15:23 +0300

    feat(minimax): add Board data class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,12 @@ enum class Marker {
     X, O
 }
 
+data class Board(
+    val boardSize: Int,
+    val state: List<Marker?>,
+    val turn: Marker,
+)
+
 fun main(args: Array<String>) {
     println("Hello World!")
 
