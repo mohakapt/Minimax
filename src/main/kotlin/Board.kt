1d325ed590211983537e00c46f0316c1bc26bc07
Haytham KATBY <mohakapt@gmail.com>
2023-06-17 15:22:57 +0300

    feat(board): add rows, columns, and diagonals extension properties

--- src/main/kotlin/Board.kt
+++ src/main/kotlin/Board.kt
@@ -6,3 +6,15 @@ data class Board(
 ) {
     val cellCount = boardSize * boardSize
 }
+
+val Board.rows: List<List<Marker?>>
+    get() = state.chunked(boardSize)
+
+val Board.columns: List<List<Marker?>>
+    get() = (0..<boardSize).map { x -> state.filterIndexed { index, _ -> index % boardSize == x } }
+
+val Board.diagonals: List<List<Marker?>>
+    get() = listOf(
+        (0..<cellCount step boardSize + 1).map { state[it] },
+        (boardSize - 1..<cellCount - boardSize + 1 step boardSize - 1).map { state[it] }
+    )
