597bd5ef0155b5c0f32f67a672fdca2ab5148b5b
Haytham KATBY <mohakapt@gmail.com>
2023-06-23 16:20:22 +0300

    feat(minimax): finish working on the search func

--- src/main/kotlin/Engine.kt
+++ src/main/kotlin/Engine.kt
@@ -1,3 +1,4 @@
+var awesomeMove = -1
 
 fun minimax(board: Board): Int {
     val boardEvaluation = board.evaluation
@@ -5,13 +6,26 @@ fun minimax(board: Board): Int {
         return boardEvaluation
     }
 
+    val maximizing = board.turn == Marker.X
+    var bestEvaluation = if (maximizing) -1 else 1
+    var bestMove = -1
+
     val moves = board.availableMoves.shuffled()
 
     for (move in moves) {
         val virtualBoard = board.makeMove(move)
         val moveEvaluation = minimax(virtualBoard)
 
+        val foundBetterMove =
+            if (maximizing) moveEvaluation > bestEvaluation
+            else moveEvaluation < bestEvaluation
+
+        if (foundBetterMove) {
+            bestEvaluation = moveEvaluation
+            bestMove = move
+        }
     }
 
-    TODO("Implement minimax algorithm.")
+    awesomeMove = bestMove
+    return bestEvaluation
 }
