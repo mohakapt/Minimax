5508b40bcd6b69665999dba7ce1adca43b143b75
Haytham KATBY <mohakapt@gmail.com>
2023-06-25 16:46:24 +0300

    chore: measure the excution time of the minimax func

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,20 @@
+import kotlin.system.measureNanoTime
+
 fun main(args: Array<String>) {
     var board = Board.empty3x3
+    var evaluation: Int
 
     board.printBoard()
-    val evaluation = minimax(board)
+    val time = measureNanoTime {
+        evaluation = minimax(board)
+    }
+
     val evaluationString = when (evaluation) {
         1 -> "win for X"
         -1 -> "win for O"
         else -> "tie"
     }
+    println("The evaluation took ${time / 1_000_000} ms.")
     println("The result for this position is a $evaluationString.")
     println("And the best move is ($awesomeMove):")
     board = board.makeMove(awesomeMove)
