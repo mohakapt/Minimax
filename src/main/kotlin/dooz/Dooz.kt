fc57f1e21e96b80083ef78baa94124e5a6b463bf
Haytham KATBY <mohakapt@gmail.com>
2023-06-26 17:00:01 +0300

    docs: document dooz' suggestMove func

--- src/main/kotlin/dooz/Dooz.kt
+++ src/main/kotlin/dooz/Dooz.kt
@@ -7,7 +7,18 @@ import dooz.model.DoozCell
 import dooz.model.Player
 import dooz.model.PlayerType
 
+/**
+ * Dooz is a Tic-Tac-Toe playing AI, created by Yamin Siahmargooei.
+ */
 internal object Dooz {
+    /**
+     * Converts our board to a format that the Dooz AI can understand, then asks the AI to suggest a move.
+     *
+     * @param board The board to suggest a move for.
+     * @param difficulty The difficulty of the AI.
+     * @return The index of the suggested move.
+     * @throws IllegalArgumentException If the cell already has an owner.
+     */
     fun suggestMove(board: Board, difficulty: AiDifficulty = AiDifficulty.Hard): Int {
         val boardSize = board.boardSize
         val playerX = Player("X", "X", 0, PlayerType.Computer)
