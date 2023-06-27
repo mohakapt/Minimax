cf84cf5b8150f86145ae38227b4849651f581283
Haytham KATBY <mohakapt@gmail.com>
2023-06-27 17:15:30 +0300

    feat: add a function to get the suggested move from charmander

---
+++ src/main/kotlin/charmander/Charmander.kt
@@ -0,0 +1,19 @@
+package charmander
+
+/**
+ * This is the first version of our engine, it's quite slow, but it should be unbeatable.
+ * I call it Charmander because it's the first evolution of the best engine I can think of.
+ * It uses the [minimax algorithm](https://en.wikipedia.org/wiki/Minimax) to find the best move.
+ */
+object Charmander {
+    /**
+     * Evaluates the given board and suggests the best move accordingly.
+     *
+     * @param board The board to evaluate.
+     * @return The index of the suggested move.
+     */
+    fun suggestMove(board: Board): Int {
+        minimax(board)
+        return awesomeMove
+    }
+}
