1891ac2014a4764f03ccb276b65675ea87817c2d
Haytham KATBY <mohakapt@gmail.com>
2023-07-03 18:30:55 +0300

    refactor(dooz): update suggestMove to the new bitboard state

--- src/main/kotlin/dooz/Dooz.kt
+++ src/main/kotlin/dooz/Dooz.kt
@@ -1,7 +1,11 @@
 package dooz
 
 import charmander.Board
+import dooz.ai.SimpleGameAi
 import dooz.model.AiDifficulty
+import dooz.model.DoozCell
+import dooz.model.Player
+import dooz.model.PlayerType
 
 /**
  * Dooz is a Tic-Tac-Toe playing AI, created by Yamin Siahmargooei.
@@ -16,6 +20,25 @@ object Dooz {
      * @throws IllegalArgumentException If the cell already has an owner.
      */
     fun suggestMove(board: Board, difficulty: AiDifficulty = AiDifficulty.Hard): Int {
-        TODO("I will have a look at this later")
+        val boardSize = board.boardSize
+        val playerX = Player("X", "X", 0, PlayerType.Computer)
+        val playerO = Player("O", "O", 1, PlayerType.Human)
+
+        val stateX: Long = board.stateX
+        val stateO: Long = board.stateO
+
+        val gameCells = List(board.cellCount) {
+            val x = it % boardSize
+            val y = it / boardSize
+            val player = when {
+                stateX and (1L shl it) != 0L -> playerX
+                stateO and (1L shl it) != 0L -> playerO
+                else -> null
+            }
+            DoozCell(x, y, player)
+        }.chunked(boardSize)
+
+        val move = SimpleGameAi(gameCells, difficulty).play()
+        return move.x + move.y * boardSize
     }
 }
