4d00dd5b5ea782cc46b891ca2ca5f3b44ebe09b1
Haytham KATBY <mohakapt@gmail.com>
2023-07-04 18:45:11 +0300

    feat: add a func to generate winning combinations for any board size

--- src/main/kotlin/charmander/Board.kt
+++ src/main/kotlin/charmander/Board.kt
@@ -64,6 +64,36 @@ private val winningCombinations = listOf(
     0b100010001L, 0b001010100L // diagonals
 )
 
+private fun getWinningCombinations(boardSize: Int): List<Long> {
+    val wins = mutableListOf<Long>()
+    val cellCount = boardSize * boardSize
+
+    for (i in 0..<cellCount) {
+        val row = i / boardSize
+        val col = i % boardSize
+
+        // Row combination
+        if (col <= 0)
+            wins.add(((1L shl boardSize) - 1) shl (row * boardSize + col))
+
+        // Column combination
+        if (row <= 0) {
+            val columnMask = (1..<boardSize).fold(1L) { acc, j -> acc or (1L shl (j * boardSize)) }
+            wins.add(columnMask shl i)
+        }
+
+        // Diagonal combination (from top-left to bottom-right)
+        if (row == col && row <= 0)
+            wins.add((1..<boardSize).fold(1L) { acc, j -> acc or (1L shl (j * (boardSize + 1))) })
+
+        // Diagonal combination (from top-right to bottom-left)
+        if (row + col == boardSize - 1 && row <= 0)
+            wins.add((1..<boardSize).fold(1L shl (boardSize - 1)) { acc, j -> acc or (1L shl ((j + 1) * (boardSize - 1))) })
+    }
+
+    return wins
+}
+
 /**
  * Checks if the board is full.
  *
