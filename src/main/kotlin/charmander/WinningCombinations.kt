f6545c199e126eb206fc2c31656b84a57a191b51
Haytham KATBY <mohakapt@gmail.com>
2023-07-05 18:53:33 +0300

    refactor: rename getWinningCombinations function to generate and put it in the object

--- src/main/kotlin/charmander/WinningCombinations.kt
+++ src/main/kotlin/charmander/WinningCombinations.kt
@@ -1,35 +1,35 @@
 package charmander
 
-fun getWinningCombinations(boardSize: Int): List<Long> {
-    val wins = mutableListOf<Long>()
-    val cellCount = boardSize * boardSize
+object WinningCombinations {
+    private fun generate(boardSize: Int): List<Long> {
+        val wins = mutableListOf<Long>()
+        val cellCount = boardSize * boardSize
 
-    for (i in 0..<cellCount) {
-        val row = i / boardSize
-        val col = i % boardSize
+        for (i in 0..<cellCount) {
+            val row = i / boardSize
+            val col = i % boardSize
 
-        // Row combination
-        if (col <= 0)
-            wins.add(((1L shl boardSize) - 1) shl (row * boardSize + col))
+            // Row combination
+            if (col <= 0)
+                wins.add(((1L shl boardSize) - 1) shl (row * boardSize + col))
 
-        // Column combination
-        if (row <= 0) {
-            val columnMask = (1..<boardSize).fold(1L) { acc, j -> acc or (1L shl (j * boardSize)) }
-            wins.add(columnMask shl i)
+            // Column combination
+            if (row <= 0) {
+                val columnMask = (1..<boardSize).fold(1L) { acc, j -> acc or (1L shl (j * boardSize)) }
+                wins.add(columnMask shl i)
+            }
+
+            // Diagonal combination (from top-left to bottom-right)
+            if (row == col && row <= 0)
+                wins.add((1..<boardSize).fold(1L) { acc, j -> acc or (1L shl (j * (boardSize + 1))) })
+
+            // Diagonal combination (from top-right to bottom-left)
+            if (row + col == boardSize - 1 && row <= 0)
+                wins.add((1..<boardSize).fold(1L shl (boardSize - 1)) { acc, j -> acc or (1L shl ((j + 1) * (boardSize - 1))) })
         }
 
-        // Diagonal combination (from top-left to bottom-right)
-        if (row == col && row <= 0)
-            wins.add((1..<boardSize).fold(1L) { acc, j -> acc or (1L shl (j * (boardSize + 1))) })
-
-        // Diagonal combination (from top-right to bottom-left)
-        if (row + col == boardSize - 1 && row <= 0)
-            wins.add((1..<boardSize).fold(1L shl (boardSize - 1)) { acc, j -> acc or (1L shl ((j + 1) * (boardSize - 1))) })
+        return wins
     }
 
-    return wins
-}
-
-object WinningCombinations {
-    fun get(boardSize: Int): List<Long> = getWinningCombinations(boardSize)
+    fun get(boardSize: Int): List<Long> = generate(boardSize)
 }
