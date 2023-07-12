ca4d505c5af053b627c1a1d4a423a2550fdf28ec
Haytham KATBY <mohakapt@gmail.com>
2023-07-12 13:22:53 +0300

    feat(dooz): remove difficulty from dooz and set it always to hard

--- src/main/kotlin/dooz/Dooz.kt
+++ src/main/kotlin/dooz/Dooz.kt
@@ -15,15 +15,14 @@ object Dooz {
      * Converts our board to a format that the Dooz AI can understand, then asks the AI to suggest a move.
      *
      * @param board The board to suggest a move for.
-     * @param difficulty The difficulty of the AI.
      * @return The index of the suggested move.
      * @throws IllegalArgumentException If the cell already has an owner.
      */
-    fun suggestMove(board: Board, difficulty: AiDifficulty = AiDifficulty.Hard): Int {
+    fun suggestMove(board: Board): Int {
         val boardSize = board.boardSize
         val gameCells = boardToGameCells(board)
 
-        val move = SimpleGameAi(gameCells, difficulty).play()
+        val move = SimpleGameAi(gameCells, AiDifficulty.Hard).play()
         return move.x + move.y * boardSize
     }
 
