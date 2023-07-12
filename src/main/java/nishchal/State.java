d582613910d7efb0628d9279594f211318686675
Haytham KATBY <mohakapt@gmail.com>
2023-07-12 13:15:51 +0300

    feat: add Nishchal engine files to play against charmeleon

---
+++ src/main/java/nishchal/State.java
@@ -0,0 +1,29 @@
+package nishchal;
+
+/**
+ * Created by Nishchal on 4/26/2017.
+ */
+public class State {
+    public String[][] board = new String[4][4];
+    public String nextPlayer;
+    int heuristicValue;
+    int depth;
+    State parentState;
+
+    public State(String[][] board, State parentState, String nextPlayer, int heuristicValue, int depth) {
+        this.board = board;
+        this.nextPlayer = nextPlayer;
+        this.heuristicValue = heuristicValue;
+        this.depth = depth;
+        this.parentState = parentState;
+    }
+
+    public State(String[][] board) {
+        this(board, null, null, 0, 0);
+    }
+
+    public State() {
+
+    }
+
+}
