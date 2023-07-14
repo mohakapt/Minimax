355feb087435069e2c7f1ce1e5b431ca3911ffeb
Haytham KATBY <mohakapt@gmail.com>
2023-07-14 14:23:58 +0300

    docs(marker): update documentation in marker enum

--- src/main/kotlin/charmeleon/Marker.kt
+++ src/main/kotlin/charmeleon/Marker.kt
@@ -2,17 +2,25 @@ package charmeleon
 
 /**
  * Represents a marker on the board.
+ *
+ * @see Board
  */
 enum class Marker {
     X, O;
 
     /**
-     * Returns the opposite marker.
+     * Retrieves the opposite marker.
      *
      * @return `X` if the marker is `O`, `O` if the marker is `X`.
      */
     val opposite: Marker
         get() = if (this == X) O else X
+
+    /**
+     * Retrieves the marker as a string.
+     *
+     * @return `"X"` if the marker is `X`, `"O"` if the marker is `O`.
+     */
     val asString: String
         get() = if (this == X) "X" else "O"
 }
