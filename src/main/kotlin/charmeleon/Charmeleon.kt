ee6c1d75401a0fbcccb7ca9439b4e9a25500f485
Haytham KATBY <mohakapt@gmail.com>
2023-07-18 18:02:13 +0300

    feat: remove global awsome move and use the new evaluation instead

--- src/main/kotlin/charmeleon/Charmeleon.kt
+++ src/main/kotlin/charmeleon/Charmeleon.kt
@@ -1,11 +1,6 @@
 package charmeleon
 
 /**
- * Stores the best move found by the engine, gets updated after calling [minimax].
- */
-internal var awesomeMove = -1
-
-/**
  * This is the first version of our engine, it's quite slow, but it should be unbeatable.
  * I call it Charmeleon because it's the second evolution of Charmander, It's still not a Charizard, but it's getting there.
  * It uses the [minimax algorithm](https://en.wikipedia.org/wiki/Minimax) to find the best move.
@@ -20,5 +15,7 @@ object Charmeleon {
     fun suggestMove(board: Board): Int {
         minimax(board)
         return awesomeMove
+        val evaluation = minimax(board)
+        return evaluation.move ?: throw IllegalStateException("No move was found, probably the board is full.")
     }
 }
