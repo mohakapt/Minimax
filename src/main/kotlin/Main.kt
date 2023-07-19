9f90489908ae4c6fc67042d8a150d14f7177c233
Haytham KATBY <mohakapt@gmail.com>
2023-07-19 19:44:03 +0300

    feat: play a few matches between the players

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,30 +1,4 @@
-import charmeleon.*
-import kotlin.system.measureNanoTime
-
 fun main(args: Array<String>) {
-//    CharmeleonVsDooz.playMatch(4, 10)
-//    testEngine()
-
-    CharmeleonVsNishchal.playMatch(30)
-}
-
-private fun testEngine() {
-    var board = Board.empty(4)
-    var evaluation: Int
-
-    board.printBoard()
-    val time = measureNanoTime {
-        evaluation = minimax(board)
-    }
-
-    val evaluationString = when (evaluation) {
-        1 -> "win for X"
-        -1 -> "win for O"
-        else -> "tie"
-    }
-    println("The evaluation took ${time / 1_000_000} ms.")
-    println("The result for this position is a $evaluationString.")
-    println("And the best move is ($awesomeMove):")
-    board = board.makeMove(awesomeMove)
-    board.printBoard()
+    Match(Player.DOOZ, Player.CHARMELEON)
+        .playMatch(3, 200)
 }
