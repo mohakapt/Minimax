b072f2710b173fc7414f91d1e15614dbf9746de0
Haytham KATBY <mohakapt@gmail.com>
2023-07-21 18:15:40 +0300

    feat(basicStrategy): play the winning move is there's a win in 1

--- src/main/kotlin/charizard/BasicStrategy.kt
+++ src/main/kotlin/charizard/BasicStrategy.kt
@@ -2,5 +2,18 @@ package charizard
 
 object BasicStrategy {
     fun suggestMove(board: Board): Int {
+        val moves = board.availableMoves
+
+        winIfPossible(board, moves)?.let { return it }
+        return moves.random()
+
+    private fun winIfPossible(board: Board, moves: List<Int>): Int? {
+        for (move in moves) {
+            val score = board.makeMove(move).score
+            val win = score == 1 && board.turn == Marker.X || score == -1 && board.turn == Marker.O
+            if (win) return move
+        }
+        return null
+    }
     }
 }
