56778214ade0769bcff83e74dfc05bc3a17f2605
Haytham KATBY <mohakapt@gmail.com>
2023-07-21 18:16:33 +0300

    feat(basicStrategy): block opponent's winning move if they have a win in 1

--- src/main/kotlin/charizard/BasicStrategy.kt
+++ src/main/kotlin/charizard/BasicStrategy.kt
@@ -5,6 +5,7 @@ object BasicStrategy {
         val moves = board.availableMoves
 
         winIfPossible(board, moves)?.let { return it }
+        blockIfNecessary(board, moves)?.let { return it }
         return moves.random()
 
     private fun winIfPossible(board: Board, moves: List<Int>): Int? {
@@ -12,6 +13,18 @@ object BasicStrategy {
             val score = board.makeMove(move).score
             val win = score == 1 && board.turn == Marker.X || score == -1 && board.turn == Marker.O
             if (win) return move
+        }
+        return null
+    }
+
+    private fun blockIfNecessary(board: Board, moves: List<Int>): Int? {
+        for (currentMove in moves) {
+            val newBoard = board.makeMove(currentMove)
+            for (nextMove in newBoard.availableMoves) {
+                val score = newBoard.makeMove(nextMove).score
+                val block = score == 1 && board.turn == Marker.O || score == -1 && board.turn == Marker.X
+                if (block) return nextMove
+            }
         }
         return null
     }
