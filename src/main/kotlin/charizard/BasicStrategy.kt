704eab06b87251564858401d1a44ab4a7b7ed7b2
Haytham KATBY <mohakapt@gmail.com>
2023-07-21 18:17:08 +0300

    feat(basicStrategy): try to take central or corner squares if possible

--- src/main/kotlin/charizard/BasicStrategy.kt
+++ src/main/kotlin/charizard/BasicStrategy.kt
@@ -6,6 +6,7 @@ object BasicStrategy {
 
         winIfPossible(board, moves)?.let { return it }
         blockIfNecessary(board, moves)?.let { return it }
+        takeCenterOrCorner(board, moves)?.let { return it }
         return moves.random()
 
     private fun winIfPossible(board: Board, moves: List<Int>): Int? {
@@ -26,6 +27,38 @@ object BasicStrategy {
                 if (block) return nextMove
             }
         }
+        return null
+    }
+
+    private fun takeCenterOrCorner(board: Board, moves: List<Int>): Int? {
+        val boardSize = board.boardSize
+        val cellCount = board.cellCount
+
+        val centers = mutableListOf<Int>()
+        if (boardSize % 2 == 1) {
+            centers.add(cellCount / 2)
+        } else {
+            centers.add(cellCount / 2 - boardSize / 2)
+            centers.add(cellCount / 2 - boardSize / 2 - 1)
+            centers.add(cellCount / 2 + boardSize / 2)
+            centers.add(cellCount / 2 + boardSize / 2 - 1)
+        }
+        for (move in centers.shuffled()) {
+            if (move in moves)
+                return move
+        }
+
+        val corners = mutableListOf(
+            0,
+            boardSize - 1,
+            boardSize * (boardSize - 1),
+            cellCount - 1,
+        )
+        for (move in corners.shuffled()) {
+            if (move in moves)
+                return move
+        }
+
         return null
     }
     }
