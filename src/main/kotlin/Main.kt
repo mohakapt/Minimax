fd67f349b9bb33c15b4bdd48d9d1fef76dd35ed8
Haytham KATBY <mohakapt@gmail.com>
2023-08-21 17:02:31 +0300

    feat: add a func to analyze the game featured on the project's cover page

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,29 @@
+import charizard.*
+
 fun main(args: Array<String>) {
     Match(Player.CHARIZARD, Player.DOOZ, printBoards = true)
-        .playMatch(8, 1)
+        .playMatch(4, 1000)
+
+    solveCoverGame()
+}
+
+val Board.Companion.coverGame: Board
+    get() = Board.empty(4)
+        .makeMove(6)    // X
+        .makeMove(2)    // O
+        .makeMove(5)    // X
+        .makeMove(0)    // O
+        .makeMove(8)    // X
+        .makeMove(1)    // O
+        .makeMove(3)    // X
+        .makeMove(14)   // O - a mistake by O, now X can force a win, any other move would have been a draw.
+
+fun solveCoverGame() {
+    var board = Board.coverGame
+    while (board.score == null) {
+        board.printBoard()
+        board = board.makeMove(Charizard.suggestMove(board))
+    }
+    board.printBoard()
+    println("X Wins!")
 }
